19ab87fb62ab46962e48e6c455115c80d4fc72ab
William Chavez <43645184+WilliamCM2D@users.noreply.github.com>
2022-09-01 11:19:32 -0600

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -1,3 +1,4 @@
+//william Alexander Chavez Marquez CM18064
 var camisa = document.querySelector('#camisa').value;
 var pantalon = document.querySelector('#pantalon').value;
 var zapato = document.querySelector('#zapato').value;
@@ -6,21 +7,44 @@ let pagar = []
 
 
 const tienda = () => {
-    const inventario = [
+    let inventario = [
         {id:1, name:"Camisa", price:5.99, stock:2},
         {id:2, name:"Pantalon", price:9.99, stock:2},
         {id:3, name:"Zapatos", price:9.30, stock:2}
     ];
 
-    const carritodecompras = [];
+    let carritodecompras = [];
+
+    function agregarProducto() {
+        document.getElementById("camisa").setAttribute("onclick","")
+        if (inventario.name == "camisa") {
+            if (inventario.stock >= 1) {
+                
+            }  
+        }
+        document.getElementById("pantalon").setAttribute("onclick","")
+        if (inventario.name == "pantalon") {
+            if (inventario.stock >= 1) {
+                
+            }  
+        }
+        document.getElementById("zapato").setAttribute("onclick","")
+        if (inventario.name == "zapato") {
+            if (inventario.stock >= 1) {
+                
+            }  
+        }
+    }
+    function realizarPago() {
+
+        
+    }
+
+    function actualizarinventario() {
+        
+        
+    }
 
     return inventario, carritodecompras;
 
-}
-
-const C1 = () => {
-    document.getElementById("camisa").setAttribute("onclick","")
-    document.getElementById("pantalon").setAttribute("onclick","")
-    document.getElementById("zapato").setAttribute("onclick","")
-    document.getElementById("pago").setAttribute("onclick","")
 }
