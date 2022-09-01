1c7e37280d355c01a5c5e53786b00ee9b2e0cb55
William Chavez <43645184+WilliamCM2D@users.noreply.github.com>
2022-09-01 10:22:42 -0600

    Add files via upload

---
+++ inventario.js
@@ -0,0 +1,16 @@
+var camisa = document.querySelector('#camisa').value;
+var pantalon = document.querySelector('#pantalon').value;
+var zapato = document.querySelector('#zapato').value;
+var pago = document.querySelector('#pago').value;
+let pagar = []
+
+let inventario = [
+    {id:1, name:"Camisa", price:5.99, stock:2},
+    {id:2, name:"Pantalon", price:9.99, stock:2},
+    {id:3, name:"Zapatos", price:9.30, stock:2}
+];
+
+
+function inventario() {
+    
+}
