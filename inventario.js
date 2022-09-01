0e61f0a98baf2e96ea7f932f0a04dc3fa50fa4b8
William Chavez <43645184+WilliamCM2D@users.noreply.github.com>
2022-09-01 10:45:34 -0600

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -4,13 +4,23 @@ var zapato = document.querySelector('#zapato').value;
 var pago = document.querySelector('#pago').value;
 let pagar = []
 
-let inventario = [
-    {id:1, name:"Camisa", price:5.99, stock:2},
-    {id:2, name:"Pantalon", price:9.99, stock:2},
-    {id:3, name:"Zapatos", price:9.30, stock:2}
-];
 
+const tienda = () => {
+    const inventario = [
+        {id:1, name:"Camisa", price:5.99, stock:2},
+        {id:2, name:"Pantalon", price:9.99, stock:2},
+        {id:3, name:"Zapatos", price:9.30, stock:2}
+    ];
 
-function inventario() {
-    
+    const carritodecompras = [];
+
+    return inventario, carritodecompras;
+
+}
+
+const C1 = () => {
+    document.getElementById("camisa").setAttribute("onclick","")
+    document.getElementById("pantalon").setAttribute("onclick","")
+    document.getElementById("zapato").setAttribute("onclick","")
+    document.getElementById("pago").setAttribute("onclick","")
 }
